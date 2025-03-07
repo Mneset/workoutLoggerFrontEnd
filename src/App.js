a51e7cda6d608b514a34e7f753727f2f076e0833
Martin Neset <marnes03358@stud.noroff.no>
2025-03-07 16:43:00 +0100

    started the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return (   
+    <div>
+      <h1>Workout App</h1>
+      <h2>Start a new session</h2>
+      <form className='start-session-form'> 
+        <label>User ID: </label>
+        <input type="number" placeholder='Please provide your User ID'/>
+        <button type="submoit">Start session</button>
+      </form>
+
+      <h2>Add exercises to your session</h2>
+      <form className='add-exercise-form'>
+        <label>Add a exercise: </label>
+        <input type="number" placeholder='Please provide the exercise ID'/>
+        <button type="submit">Add exercise</button>
+      </form>
+
+      <h2>End session</h2>
+      <form className='end-session-form'> 
+        <button type="submoit">End session</button>
+      </form>
+
+      <h2>Session Log</h2>
+    </div>   
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,6 @@
+const axios = requie('axios')
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000'
+});
+
